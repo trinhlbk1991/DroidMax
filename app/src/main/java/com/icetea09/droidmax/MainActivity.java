2463263a559a11df03913f60425075b8fd3f3979
Trinh Le <trinhlbk1991@gmail.com>
2016-01-09 19:27:39 +0700

    [f] Implement Add New Rule Fragment

--- app/src/main/java/com/icetea09/droidmax/MainActivity.java
+++ app/src/main/java/com/icetea09/droidmax/MainActivity.java
@@ -1,20 +1,48 @@
 package com.icetea09.droidmax;
 
 import android.os.Bundle;
+import android.support.annotation.AnimRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-import com.icetea09.droidmax.actions.BluetoothAction.BlueToothAction;
-import com.icetea09.droidmax.actions.ChangeWifiAction.WifiAction;
-import com.icetea09.droidmax.actions.GPSAction.GpsAction;
+import com.icetea09.droidmax.fragment.AddNewRuleFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private FragmentManager mFragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mFragmentManager = getSupportFragmentManager();
+        setFragment(AddNewRuleFragment.newInstance(), true);
     }
+
+    public void setFragment(Fragment fragment, boolean isAddToBackStack) {
+        setFragment(fragment, isAddToBackStack, R.anim.slide_in_from_right, R.anim.slide_out_to_left,
+                R.anim.slide_in_from_left, R.anim.slide_out_to_right);
+    }
+
+    public void setFragment(Fragment fragment, boolean isAddToBackStack,
+                            @AnimRes int enter, @AnimRes int exit, @AnimRes int popEnter,
+                            @android.support.annotation.AnimRes int popExit) {
+        try {
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            fragmentTransaction.setCustomAnimations(enter, exit, popEnter, popExit);
+            fragmentTransaction.replace(R.id.fragment_container, fragment, fragment.getClass().getSimpleName());
+            if (isAddToBackStack) {
+                fragmentTransaction.addToBackStack(fragment.getClass().getSimpleName());
+            }
+            fragmentTransaction.commit();
+        } catch (Exception ex) {
+            Log.e(TAG, ex.getMessage());
+        }
+    }
+
 }
