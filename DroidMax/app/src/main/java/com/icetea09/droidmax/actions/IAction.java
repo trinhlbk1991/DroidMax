53a1032e96ea4a8e8f057d289d3a0762f75c4c59
Trinh Le <trinhlbk1991@gmail.com>
2016-01-09 10:52:06 +0700

    [f] Implement rule for Battery Added IAction and IRule interfaces.

---
+++ DroidMax/app/src/main/java/com/icetea09/droidmax/actions/IAction.java
@@ -0,0 +1,8 @@
+package com.icetea09.droidmax.actions;
+
+/**
+ * Created by KhanhTrinh on 1/9/2016.
+ */
+public interface IAction {
+    void perform();
+}
---
+++ DroidMax/app/src/main/java/com/icetea09/droidmax/rules/IRule.java
@@ -0,0 +1,8 @@
+package com.icetea09.droidmax.rules;
+
+/**
+ * Created by KhanhTrinh on 1/9/2016.
+ */
+public interface IRule {
+    boolean isSatisfied();
+}
